4d9b9046ead8e5f65932f2e58b96a96151b5b809
ZeninPhamDev <zenin3958@gmail.com>
2024-03-14 10:18:46 +0700

    sample products

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react'
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -8,7 +9,19 @@ function App() {
     <div>
       <header>Zenin Shop</header>
 
-      <main></main>
+      <main>
+        {sampleProducts.map((product) => (
+          <li key={product.slug}>
+            <img
+              src={product.image}
+              alt={product.name}
+              className="product-image"
+            />
+            <h2>{product.name}</h2>
+            <p>${product.price}</p>
+          </li>
+        ))}
+      </main>
 
       <footer>All rights revsered</footer>
     </div>
