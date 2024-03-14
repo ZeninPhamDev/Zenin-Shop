155375fd01d721d6497b1c8351baa8dd27179f32
ZeninPhamDev <zenin3958@gmail.com>
2024-03-14 12:51:14 +0700

    Add React router dom

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,11 +1,27 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import HomePage from './pages/HomePage.tsx'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} element={<HomePage />}></Route>
+      {/* <Route path='dashboard' element={<Dashboard />} />
+      ... etc. */}
+    </Route>,
+  ),
+)
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
