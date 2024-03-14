9ab3dec89fe3388b438ca9c90417433241acbfc8
ZeninPhamDev <zenin3958@gmail.com>
2024-03-14 20:38:31 +0700

    Make dark light theme by React Context

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -4,7 +4,7 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
@@ -14,6 +14,7 @@ import ProductPage from './pages/ProductPage.tsx'
 import { HelmetProvider } from 'react-helmet-async'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { StoreProvider } from './Store.tsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -30,11 +31,13 @@ const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </HelmetProvider>
+    <StoreProvider>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </HelmetProvider>
+    </StoreProvider>
   </React.StrictMode>,
 )
