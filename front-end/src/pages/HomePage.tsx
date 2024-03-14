a811e1a2f268b56a5e2ca04d8852c9c654036ba1
ZeninPhamDev <zenin3958@gmail.com>
2024-03-14 17:29:15 +0700

    Create Product Page

--- front-end/src/pages/HomePage.tsx
+++ front-end/src/pages/HomePage.tsx
@@ -6,7 +6,7 @@ import LoadingBox from '../components/LoadingBox'
 import MessageBox from '../components/MessageBox'
 import ProductItem from '../components/ProductItem'
 import { Helmet } from 'react-helmet-async'
-import { useGetProductsQuery } from '../hooks/productHook'
+import { useGetProductsQuery } from '../hooks/productHooks'
 
 export default function HomePage() {
   const { data: products, isLoading, error } = useGetProductsQuery()
