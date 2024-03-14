e8e693c11ff6bd16e533e981753c876ecad7fea2
ZeninPhamDev <zenin3958@gmail.com>
2024-03-14 16:16:07 +0700

    Add Helmet React Async

--- front-end/src/pages/HomePage.tsx
+++ front-end/src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Col, Row } from 'react-bootstrap'
 import { sampleProducts } from '../data'
-import { Link } from 'react-router-dom'
 import { Product } from '../types/Product'
 import { useEffect, useReducer } from 'react'
 import { getError } from '../utils'
@@ -10,6 +9,7 @@ import axios from 'axios'
 import LoadingBox from '../components/LoadingBox'
 import MessageBox from '../components/MessageBox'
 import ProductItem from '../components/ProductItem'
+import { Helmet } from 'react-helmet-async'
 
 type State = {
   products: Product[]
@@ -68,6 +68,9 @@ export default function HomePage() {
     <MessageBox variant='danger'>{error}</MessageBox>
   ) : (
     <Row>
+      <Helmet>
+        <title>Zenin Shop</title>
+      </Helmet>
       {sampleProducts.map((product) => (
         <Col key={product.slug} sm={6} md={4} lg={3}>
           <ProductItem product={product} />
